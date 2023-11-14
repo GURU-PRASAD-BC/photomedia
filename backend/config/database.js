c477e2f12f82bcf521d0308902ba097d6bf0ab17
GURU-PRASAD-BC <guruprasadbc.21it@kongu.edu>
2023-11-14 14:34:22 +0530

    first

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    mongoose.connect("mongodb+srv://Jithin_K_007:jithin1204@healthassistant.fzuwvng.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongoose Connected");
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+module.exports = connectDatabase;
+
+// mongodb+srv://guruprasadbc:guru2003@cluster0.qkaikxf.mongodb.net/?retryWrites=true&w=majority
